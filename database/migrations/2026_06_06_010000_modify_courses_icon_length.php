ad1111d791d6d80a481cdfd38ca6b43b5e322815
yastar123 <eduj2164@gmail.com>
2026-06-09 16:23:26 +0700

    SCI

--- database/migrations/2026_06_06_010000_modify_courses_icon_length.php
+++ database/migrations/2026_06_06_010000_modify_courses_icon_length.php
@@ -2,16 +2,27 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Config;
 
 return new class extends Migration
 {
     public function up()
     {
-        DB::statement('ALTER TABLE courses ALTER COLUMN icon TYPE VARCHAR(255)');
+        $driver = Config::get('database.default');
+        if ($driver === 'mysql') {
+            DB::statement('ALTER TABLE courses MODIFY icon VARCHAR(255)');
+        } elseif ($driver === 'pgsql') {
+            DB::statement('ALTER TABLE courses ALTER COLUMN icon TYPE VARCHAR(255)');
+        }
     }
 
     public function down()
     {
-        DB::statement('ALTER TABLE courses ALTER COLUMN icon TYPE VARCHAR(50)');
+        $driver = Config::get('database.default');
+        if ($driver === 'mysql') {
+            DB::statement('ALTER TABLE courses MODIFY icon VARCHAR(50)');
+        } elseif ($driver === 'pgsql') {
+            DB::statement('ALTER TABLE courses ALTER COLUMN icon TYPE VARCHAR(50)');
+        }
     }
 };
